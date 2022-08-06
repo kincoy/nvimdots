9311796bda8a13d2f82b162d0c1e3fc201735a72
kincoy <1152072645@qq.com>
2022-08-06 21:40:42 +0800

    feat(modules): set tools plugins

--- lua/keymap/plugins.lua
+++ lua/keymap/plugins.lua
@@ -8,7 +8,26 @@ local plugins_map = {
     -- Plugin nvim-tree
     ["n|<C-n>"] = map_cr("NvimTreeToggle"):with_noremap():with_silent(),
     ["n|<Leader>nf"] = map_cr("NvimTreeFindFile"):with_noremap():with_silent(),
-    ["n|<Leader>nr"] = map_cr("NvimTreeRefresh"):with_noremap():with_silent()
+    ["n|<Leader>nr"] = map_cr("NvimTreeRefresh"):with_noremap():with_silent(),
+    -- Plugin Telescope
+    ["n|<Leader>fp"] = map_cu(
+        "lua require('telescope').extensions.project.project{}"):with_noremap()
+        :with_silent(),
+    ["n|<Leader>fr"] = map_cu(
+        "lua require('telescope').extensions.frecency.frecency{}"):with_noremap()
+        :with_silent(),
+    ["n|<Leader>fe"] = map_cu("Telescope oldfiles"):with_noremap():with_silent(),
+    ["n|<Leader>ff"] = map_cu("Telescope find_files"):with_noremap()
+        :with_silent(),
+    ["n|<Leader>sc"] = map_cu("Telescope colorscheme"):with_noremap()
+        :with_silent(),
+    ["n|<Leader>fn"] = map_cu(":enew"):with_noremap():with_silent(),
+    ["n|<Leader>fw"] = map_cu("Telescope live_grep"):with_noremap()
+        :with_silent(),
+    ["n|<Leader>fg"] = map_cu("Telescope git_files"):with_noremap()
+        :with_silent(),
+    ["n|<Leader>fz"] = map_cu("Telescope zoxide list"):with_noremap()
+        :with_silent()
 }
 
 return plugins_map
--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -187,11 +187,19 @@ end
 
 function config.nvim_tree()
     require("nvim-tree").setup({
-        git = {enable = true},
-        -- project plugin 需要这样设置
+        respect_buf_cwd = true,
+        auto_reload_on_write = true,
+        disable_netrw = false,
+        hijack_cursor = true,
+        hijack_netrw = true,
+        hijack_unnamed_buffer_when_opening = false,
+        ignore_buffer_on_setup = false,
+        open_on_setup = false,
+        open_on_setup_file = false,
+        open_on_tab = false,
+        sort_by = "name",
         update_cwd = true,
         update_focused_file = {enable = true, update_cwd = true},
-        sort_by = "name",
         view = {
             width = 30,
             height = 30,
@@ -209,24 +217,6 @@ function config.nvim_tree()
                     {key = "a", action = "create"},
                     {key = "d", action = "remove"},
                     {key = "r", action = "rename"}
-                }
-            }
-        },
-        filters = {dotfiles = false, custom = {".DS_Store"}, exclude = {}},
-        actions = {
-            open_file = {
-                quit_on_open = false,
-                resize_window = false,
-                window_picker = {
-                    enable = true,
-                    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-                    exclude = {
-                        filetype = {
-                            "notify", "packer", "qf", "diff", "fugitive",
-                            "fugitiveblame"
-                        },
-                        buftype = {"nofile", "terminal", "help"}
-                    }
                 }
             }
         },
@@ -265,7 +255,35 @@ function config.nvim_tree()
                     }
                 }
             }
+        },
+        hijack_directories = {enable = true, auto_open = true},
+        update_focused_file = {
+            enable = true,
+            update_cwd = true,
+            ignore_list = {}
+        },
+        ignore_ft_on_setup = {},
+        filters = {dotfiles = false, custom = {".DS_Store"}, exclude = {}},
+        actions = {
+            use_system_clipboard = true,
+            change_dir = {enable = true, global = false},
+            open_file = {
+                quit_on_open = false,
+                resize_window = false,
+                window_picker = {
+                    enable = true,
+                    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+                    exclude = {
+                        filetype = {
+                            "notify", "packer", "qf", "diff", "fugitive",
+                            "fugitiveblame"
+                        },
+                        buftype = {"nofile", "terminal", "help"}
+                    }
+                }
+            }
         }
+
     })
 end
 
